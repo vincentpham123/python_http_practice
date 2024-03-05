865127a85eea95ebd280a7a0e9f88ab78d439bd1
vincentpham123 <vincentpham32@gmail.com>
2024-03-04 17:16:47 -0800

    messing around with urllib

--- urllib_practice.py
+++ urllib_practice.py
@@ -1,5 +1,10 @@
 import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+fhand = urllib.request.urlopen('http://data.pr4e.org/')
+
+counts = {}
 for line in fhand:
-    print(line.decode().strip())
+    words = line.decode().split()
+
+    
+
