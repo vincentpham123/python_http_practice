c47c48080ddc66dc568d53186ea9aad47b107644
vincentpham123 <vincentpham32@gmail.com>
2024-03-06 14:46:36 -0800

    beautiful soup problem

--- scraping_numbers.py
+++ scraping_numbers.py
@@ -1,17 +1,21 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
-
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+url = input('Enter -')
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+html = urllib.request.urlopen(url, context=ctx).read()
+
+soup =BeautifulSoup(html, 'html.parser')
+
+spans = soup('span')
+
+sum = 0 
+
+for span in spans:
+    number = int(span.contents[0])
+    sum += number 
+print(sum)
